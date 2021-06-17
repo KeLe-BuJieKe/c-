2e75532de89aafe26a622989c48cb5ebb3170bb5
杰 周 <1941286082@qq.com>
2021-06-17 22:32:58 +0800

    -

--- STL-常用容器/Exercises/string_exercises/string_exercises.cpp
+++ STL-常用容器/Exercises/string_exercises/string_exercises.cpp
@@ -1,5 +1,6 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
+#include<vector>
 using  namespace std;
 
 
@@ -161,3 +162,153 @@ public:
 		return -1;
 	}
 };
+
+
+
+/*5.把字符串转换成整数
+描述:将一个字符串转换成一个整数，要求不能使用字符串转换整数的库函数。 数值为0或者字符串不是一个合法的数值则返回0
+输入描述：输入一个字符串,包括数字字母符号,可以为空
+返回值描述：如果是合法的数值表达则返回该数字，否则返回0
+示例1
+输入："+2147483647"
+返回值：2147483647
+示例2
+输入："1a33"
+返回值：0
+
+*/
+class Solution
+{
+public:
+	int StrToInt(string str)
+	{
+		//1.先判断该字符串是否为空，如果为空则返回0，否则执行下面语句
+		if (str.size() == 0)
+		{
+			return 0;
+		}
+		int flag = 1;         //默认该数是正数
+		int i = 0;            //下标
+		//2.判断是正数还是负数
+		//如果该字符串是正数且第一个没有+号时，则默认是使用正数
+		if (str[i] == '-')
+		{
+			flag = -1;
+			i++;
+		}
+		else if (str[i] == '+')
+		{
+			flag = 1;
+			i++;
+		}
+
+		int num = 0;        //返回的结果值
+		for (; i < str.size(); i++)
+		{
+			//我们的数字是从0-9前闭后闭的一个区间
+			//如果不是在这个区间内，则说明该字符中有其他的字符，就返回0
+			if (str[i] >= '0' && str[i] <= '9')
+			{
+				//每遍历一个在0-9间的字符，就将其加到num中
+				num = num * 10 + flag * (str[i] - '0');
+			}
+			else
+			{
+				return 0;
+			}
+			//判断该数是否造成溢出，如果溢出了，则要返回0
+			if (((num > 0x7fffffff) && (flag == 1)) || ((flag == -1) && (num < 0x80000000)))
+			{
+				return 0;
+			}
+		}
+		return num;
+	}
+};
+
+/*6.反转字符串
+编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+示例 1：
+输入：["h","e","l","l","o"]
+输出：["o","l","l","e","h"]
+
+示例 2：
+输入：["H","a","n","n","a","h"]
+输出：["h","a","n","n","a","H"]
+链接：https://leetcode-cn.com/problems/reverse-string/
+*/
+
+class Solution 
+{
+public:
+	void reverseString(vector<char>& s)
+	{
+		int begin = 0;
+		int end = s.size() - 1;
+		while (begin < end)
+		{
+			swap(s[begin], s[end]);
+			begin++;
+			end--;
+		}
+	}
+};
+
+
+
+/*7.验证回文串
+给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+说明：本题中，我们将空字符串定义为有效的回文串。
+示例 1:
+输入: "A man, a plan, a canal: Panama"
+输出: true
+
+示例 2:
+输入: "race a car"
+输出: false
+
+链接：https://leetcode-cn.com/problems/valid-palindrome/
+*/
+//方法一：可以通过但是效率低
+//效率低的主要原因是删除的时候，要挪动数据
+bool isPalindrome(string s)
+{
+	for (int i = 0; i < s.size();)
+	{
+		if (s[i] >= 'A' && s[i] <= 'Z')
+		{
+			s[i] += 32;
+			i++;
+		}
+		else if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			i++;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			i++;
+		}
+		else
+		{
+			s.erase(i, 1);
+		}
+	}
+	int begin = 0;
+	int end = s.size() - 1;
+	while (begin < end)
+	{
+		if (s[begin] != s[end])
+		{
+			return false;
+
+		}
+		begin++;
+		end--;
+	}
+	return true;
+}
+
+
+//方法二：
