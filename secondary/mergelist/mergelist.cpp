2b1dda9d17b7903e9e5b40953968fecce228b604
Pco123 <1941286082@qq.com>
2021-10-03 19:43:24 +0800

    合并两个链表

---
+++ secondary/mergelist/mergelist.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+/*1669. 合并两个链表
+给你两个链表 list1 和 list2 ，它们包含的元素分别为 n 个和 m 个。
+请你将 list1 中下标从 a 到 b 的全部节点都删除，并将list2 接在被删除节点的位置。
+下图中蓝色边和节点展示了操作后的结果：
+请你返回结果链表的头指针。
+示例 1：
+输入：list1 = [0,1,2,3,4,5], a = 3, b = 4, list2 = [1000000,1000001,1000002]
+输出：[0,1,2,1000000,1000001,1000002,5]
+解释：我们删除 list1 中下标为 3 和 4 的两个节点，并将 list2 接在该位置。上图中蓝色的边和节点为答案链表。
+
+示例 2：
+输入：list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 = [1000000,1000001,1000002,1000003,1000004]
+输出：[0,1,1000000,1000001,1000002,1000003,1000004,6]
+解释：上图中蓝色的边和节点为答案链表。
+提示：
+3 <= list1.length <= 104
+1 <= a <= b < list1.length - 1
+1 <= list2.length <= 104
+*/
+
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution 
+{
+public:
+    //这里的a与b在题目中做了限制，不管怎么样，list1链表的头和尾部一定不会被删除
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) 
+    {
+        ListNode*list1removepre=nullptr;
+        ListNode*cur=list1;
+        b-=a;
+        // 找到list1中第a个节点的前一个节点
+        while(a--)
+        {
+            list1removepre=cur;
+            cur=cur->next;
+        }
+        // 链接上list2的头
+        list1removepre->next=list2;
+        // 找到list1中第b个节点的后一个节点
+        ListNode*list1removenext=cur->next;
+        while(b--)
+        {
+            delete cur;
+            cur=list1removenext;
+            list1removenext=list1removenext->next;
+        }
+        // 找到list2中的链表尾
+        ListNode*list2tail=list2;
+        while(list2tail->next!=nullptr)
+        {
+            list2tail=list2tail->next;
+        }
+        
+        list2tail->next=list1removenext;
+        return list1;
+    }
+};
+
+int main()
+{
+
+  return 0;
+}
